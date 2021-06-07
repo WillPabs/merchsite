b7859f429ecf5bd79db5a7c1fb3574e863053d86
William <pabiterowilliam@yahoo.com>
2021-06-07 17:53:27 -0400

    Updated addProduct method to return a ResponseEntity

--- src/main/java/com/pabitero/merchsite/controller/ProductController.java
+++ src/main/java/com/pabitero/merchsite/controller/ProductController.java
@@ -3,54 +3,62 @@ package com.pabitero.merchsite.controller;
 import com.pabitero.merchsite.entity.Product;
 import com.pabitero.merchsite.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/products")
 public class ProductController {
 
     @Autowired
     private ProductService service;
 
-    @GetMapping("/")
-    public String goToIndex() {
-        return "index";
-    }
-
-    @PostMapping("/addProduct")
-    public Product addProduct(@RequestBody Product product) {
-        return service.saveProduct(product);
-    }
-
-    @PostMapping("/addProducts")
-    public List<Product> addProducts(@RequestBody List<Product> products) {
-        return service.saveProducts(products);
-    }
-
-    @GetMapping("/products")
-    public List<Product> findAllProducts() {
+    @GetMapping()
+    public List<Product> findAllProducts(@RequestParam(value = "page", defaultValue = "1") int page,
+                                         @RequestParam(value = "limit", defaultValue = "10") int limit,
+                                         @RequestParam(value = "sort", defaultValue = "desc", required = false) String sort)  {
         return service.getProducts();
     }
 
-    @GetMapping("/products/{id}")
-    public Product findProductById(@PathVariable int id) {
-        return service.getProductById(id);
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> findProductById(@PathVariable int id) {
+        Optional<Product> product = Optional.ofNullable(service.getProductById(id));
+        return product.map(response -> ResponseEntity.ok().body(response))
+                        .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
-    @GetMapping("/products/{name}")
-    public Product findProductByName(@PathVariable   String name) {
+    @GetMapping("/{name}")
+    public Product findProductByName(@PathVariable String name) {
         return service.getProductByName(name);
     }
 
-    @PutMapping("/update")
-    public Product updateProduct(@RequestBody Product product) {
-        return service.updateProduct(product);
+    @PostMapping()
+    public ResponseEntity<Product> addProduct(@RequestBody Product product) throws URISyntaxException {
+        Product result = service.saveProduct(product);
+        return ResponseEntity.created(new URI("/products" + result.getId())).body(result);
     }
 
-    @DeleteMapping("/delete/{id}")
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@RequestBody Product product, @PathVariable("id") int id) {
+        Product isExists = service.getProductById(id);
+        if(isExists == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else if(product == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        service.updateProduct(product);
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
     public String deleteProduct(@PathVariable int id) {
-        return service.deleteProduct(id );
+        return service.deleteProduct(id);
     }
 }
