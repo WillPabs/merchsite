5ea83ab9c53fd41023c5a07367ec7d32062f278f
William <pabiterowilliam@yahoo.com>
2021-08-27 12:34:04 -0400

    Connected api to frontend and displayed JSON to landing page

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,16 +1,56 @@
 import axios from 'axios'
-import { useState } from 'react';
+import { Component, useState } from 'react';
 
-function App() {
-  const [users, setUsers] = useState('');
-  axios.get('http://localhost:3307/users/3').then( res => setUsers(res.data))
+const usersApi = axios.create({
+  baseURL: `http://localhost:3307/users/`
+})
 
-  return (
-    <div className="App">
-      <h1> Hello </h1>
-      <p>{users.firstName}</p>
-    </div>
-  );
+const productsApi = axios.create({
+  baseURL: `http://localhost:3307/products/`
+})
+
+class App extends Component {
+
+  state = {
+    users: [],
+    products: []
+  }
+
+  constructor() {
+    super();
+    usersApi.get('/').then(res => {
+      console.log(res.data)
+      this.setState({ users: res.data })
+    });
+    productsApi.get('/').then(res => {
+      console.log(res.data)
+      this.setState({ products: res.data})
+    });
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <h1> Users </h1>
+        {this.state.users.map(user => 
+          <div key={user.id}>
+            <h4>{user.firstName} {user.lastName}</h4>
+            <p>Email: {user.email}</p>
+            <p>Address: {user.address}</p>
+          </div>
+        )}
+        <h1> Products </h1>
+        {this.state.products.map(product => 
+          <div key={product.id}>
+            <h4>{product.name}</h4>
+            <p>${product.price}</p>  
+            <p>Quantity: {product.quantity}</p>
+            <p>{product.description}</p>
+          </div>
+        )}
+      </div>
+    );  
+  }
 }
 
 export default App;
