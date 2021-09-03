e089ea6e0ed19fd2b30623b3881e5283d2229e89
William <pabiterowilliam@yahoo.com>
2021-09-03 17:09:47 -0400

    Changed components from class to functional

--- src/main/frontend/src/components/Products.js
+++ src/main/frontend/src/components/Products.js
@@ -1,39 +1,69 @@
-import { Component } from 'react'
+import { Component, useState, useEffect } from 'react'
 import axios from 'axios'
 
 const productsApi = axios.create({
     baseURL: `http://localhost:3307/products/`
 })
 
-class Products extends Component {
+const Products = () => {
+    const [products, setProducts] = useState([])
 
-    state = {
-        products: []
-    }
-
-    constructor() {
-        super()
+    useEffect(() => {
         productsApi.get('/').then(res => {
+            setProducts(res.data)
             console.log(res.data)
-            this.setState({products: res.data})
         })
-    }
-    
-    render() {
-        return (
-            <div className="products-list">
-                <h1>Products</h1>
-                {this.state.products.map(product =>
-                    <div key={product.id}>
-                        <h4><a href={'http://localhost:3307/products/' + product.id}>{product.name}</a></h4>
-                        <span>${product.price}</span><br/>
-                        <span>Quantity: {product.quantity}</span><br/>
-                        <span>{product.description}</span><br/>
-                    </div>
-                )}
-            </div>
-        )
-    }
+        
+    }, [])
+
+    return (
+        <div className="products-list">
+            <h1>Products</h1>
+            {products.map(product =>
+                <div key={product.id} className="product card">
+                    <h4><a href={'http://localhost:3307/products/' + product.id}>{product.name}</a></h4>
+
+                    {/* <Link to = {`/products/${product.id}} */}
+                    <span>${product.price}</span><br/>
+                    <span>Quantity: {product.quantity}</span><br/>
+                    <span>{product.description}</span><br/>
+                </div>
+            )}
+        </div>
+    )
 }
+
+// class Products extends Component {
+
+//     state = {
+//         products: []
+//     }
+
+//     constructor() {
+//         super()
+//         productsApi.get('/').then(res => {
+//             console.log(res.data)
+//             this.setState({products: res.data})
+//         })
+//     }
+    
+//     render() {
+//         return (
+//             <div className="products-list">
+//                 <h1>Products</h1>
+//                 {this.state.products.map(product =>
+//                     <div key={product.id} className="product card">
+//                         <h4><a href={'http://localhost:3307/products/' + product.id}>{product.name}</a></h4>
+
+//                         {/* <Link to = {`/products/${product.id}} */}
+//                         <span>${product.price}</span><br/>
+//                         <span>Quantity: {product.quantity}</span><br/>
+//                         <span>{product.description}</span><br/>
+//                     </div>
+//                 )}
+//             </div>
+//         )
+//     }
+// }
 
 export default Products
--- src/main/frontend/src/components/Users.js
+++ src/main/frontend/src/components/Users.js
@@ -1,38 +1,62 @@
 import axios from 'axios'
-import { Component } from 'react'
+import { Component, useEffect, useState } from 'react'
 
 const usersApi = axios.create({
     baseURL: `http://localhost:3307/users/`
 })
 
-class Users extends Component {
+const Users = () => {
+    const [users, setUsers] = useState([])
 
-    state = {
-        users: []
-    }
-
-    constructor(props) {
-        super()
+    useEffect(() => {
         usersApi.get('/').then(res => {
+            setUsers(res.data)
             console.log(res.data)
-            this.setState({users: res.data})
         })
-    }
+    }, [])
 
-    render() {
-        return (
-            <div className="users-list">
-                <h1>Users</h1>
-                {this.state.users.map(user => 
-                    <div key={user.id}>
-                        <h4>{user.firstName} {user.lastName}</h4>
-                        <p>Email: {user.email}</p>
-                        <p>Address: {user.address}</p>
-                    </div>    
-                )}
-            </div>
-        )
-    }
+    return (
+        <div className="users-list">
+            <h1>Users</h1>
+            {users.map(user => 
+                <div key={user.id}>
+                    <h4>{user.firstName} {user.lastName}</h4>
+                    <p>Email: {user.email}</p>
+                    <p>Address: {user.address}</p>
+                </div>    
+            )}
+        </div>
+    )
 }
+
+// class Users extends Component {
+
+//     state = {
+//         users: []
+//     }
+
+//     constructor(props) {
+//         super()
+//         usersApi.get('/').then(res => {
+//             console.log(res.data)
+//             this.setState({users: res.data})
+//         })
+//     }
+
+//     render() {
+//         return (
+//             <div className="users-list">
+//                 <h1>Users</h1>
+//                 {this.state.users.map(user => 
+//                     <div key={user.id}>
+//                         <h4>{user.firstName} {user.lastName}</h4>
+//                         <p>Email: {user.email}</p>
+//                         <p>Address: {user.address}</p>
+//                     </div>    
+//                 )}
+//             </div>
+//         )
+//     }
+// }
 
 export default Users
