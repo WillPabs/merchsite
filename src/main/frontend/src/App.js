4f1ec5b8df6ea569360283e1c26e4197667d9393
William <pabiterowilliam@yahoo.com>
2021-09-15 18:35:35 -0400

    Added Router functionality

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,20 +1,30 @@
-import { Component, useState } from 'react';
+import { Component } from 'react';
+import { Route } from 'react-router-dom';
 import Navbar from './components/Navbar'
 import Products from './components/Products'
 import Users from './components/Users'
+import AddUser from './components/AddUser'
+import AddProduct from './components/AddProduct';
+import Login from './components/Login';
+
 
 
 class App extends Component {
   
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <div className="main">
-          <Users />
-          <Products />
+      <main>
+        <div className="App">
+          <Navbar />
+          <div className="main">
+            {/* <AddUser/>
+            <Users/>
+            <AddProduct/>
+            <Products/>
+            <Login/> */}
+          </div>
         </div>
-      </div>
+      </main>
     );  
   }
 }
--- src/main/frontend/src/components/Navbar.js
+++ src/main/frontend/src/components/Navbar.js
@@ -1,17 +1,43 @@
+import SearchBar from "./SearchBar";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import Users from "./Users";
+import Products from "./Products";
+import About from "./About";
+import Contact from "./Contact"
+import Login from "./Login";
+import AddUser from "./AddUser";
+import Home from "./Home";
+
 const logo = "https://www.logolynx.com/images/logolynx/b2/b23666d57f4e17102209423105dfc442.png"
 
 const Navbar = () => {
     return (
-        <nav className="navbar">
-            <a href="#logo"><img src={logo} alt="logo"/></a>
-            <ul>
-                <li><a href="#home">Home</a></li>
-                <li><a href="#users">Users</a></li>
-                <li><a href="#products">Products</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#contact">Contact</a></li>
-            </ul>
-        </nav>
+        <Router>
+            <nav className="navbar">
+                <a href="#logo"><img src={logo} alt="logo"/></a>
+                <ul className="navbar-left">
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/users">Users</Link></li>
+                    <li><Link to="/products">Products</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
+                </ul>
+                <ul className="navbar-right">
+                    <li><Link to="/login">Login</Link></li>
+                    <li><Link to="/register">Sign Up</Link></li>
+                </ul>
+                <SearchBar/>
+            </nav>
+            <div className="routes">
+                <Route exact path="/" component={Home}/>
+                <Route path="/users" component={Users}/>
+                <Route path="/products" component={Products}/>
+                <Route path="/about" component={About}/>
+                <Route path="/contact" component={Contact}/>
+                <Route path="/login" component={Login}/>
+                <Route path="/register" component={AddUser}/>
+            </div>
+        </Router>
     )
 }
 
